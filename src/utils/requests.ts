0d397f9a7096f80493809c0a05d41025ab4dd65a
hosyaeuw <gad972@mail.ru>
2024-03-02 14:29:09 +0300

    i am idiot

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,8 +1,8 @@
-import got from "got";
+import got from 'got';
 
 const request = got.extend({
-  prefixUrl: "https://data.dbmovies.org",
-  headers: { "user-agent": "node-moviedb/0.2.5" },
+  prefixUrl: 'https://data.dbmovies.org',
+  headers: { 'user-agent': 'node-moviedb/0.2.5' },
 });
 
 export default request;
