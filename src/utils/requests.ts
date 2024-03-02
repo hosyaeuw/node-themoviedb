3375d8930684e3b8d31484a286e625727ad8e1d4
hosyaeuw <gad972@mail.ru>
2024-03-02 14:05:04 +0300

    refactor: change requests options

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,8 +1,7 @@
-import got from 'got';
+import got from "got";
 
 const request = got.extend({
-  prefixUrl: 'https://api.themoviedb.org/3',
-  headers: { 'user-agent': 'node-moviedb/0.2.5' },
+  prefixUrl: "https://data.dbmovies.org",
 });
 
 export default request;
